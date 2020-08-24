1f9f62906ebbf2368e054bd3968b9a102c89c47b
marceloagustini <agustinimarcelo@gmail.com>
2020-08-24 14:12:21 -0300

    update and add postgres datasource

---
+++ src/datasources/postgres.datasource.ts
@@ -0,0 +1,31 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+
+const config = {
+  name: 'postgres',
+  connector: 'postgresql',
+  url: 'localhost',
+  host: 'localhost',
+  port: 5432,
+  user: 'postgres',
+  password: 'postgres',
+  database: 'examplejwt'
+};
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class PostgresDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'postgres';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.postgres', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
